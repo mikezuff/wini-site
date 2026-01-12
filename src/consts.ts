87acac4d762270214e303bd606c754713d10f18a
Michael Zuffoletti <mikezuff@gmail.com>
2026-01-11 21:52:44 -0800

    highlight cookie link, deemphasize home page, polish

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Wini's Website";
-export const SITE_DESCRIPTION = "Welcome to Wini's website!";
+export const SITE_TITLE = "Wini's Digital Cookie Booth";
+export const SITE_DESCRIPTION = "Welcome to Wini's digital cookie booth!";
